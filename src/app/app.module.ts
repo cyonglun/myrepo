395295e7398ea290515f9dbdb1131a41384f7214
Yong Lun <chua_yong_lun@tech.gov.sg>
2022-12-06 12:26:33 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DailyQuoteComponent } from './daily-quote/daily-quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DailyQuoteComponent
   ],
   imports: [
     BrowserModule,
